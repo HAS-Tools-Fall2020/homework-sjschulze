da4b63e42752bfe64c3faa7e333b1e042ed6951f
Scott Schulze <sjschulze@mail.arizona.edu>
2020-11-01 22:22:14 -0700

    updating with the week's work

---
+++ homework_10/Schulze_map.py
@@ -0,0 +1,28 @@
+# Scott Schulze
+# HAS Tools Homwork week 10
+# 10/30/2020
+# Plotting a map using GIS tools in Python
+
+# %%
+# Import libraries
+
+import matplotlib.pyplot as plt
+import numpy as np
+import geopandas as gpd
+import fiona
+from shapely.geometry import Point
+import contextily as ctx
+import os
+
+# %%
+# Reading in  the shapefile using geopandas
+file = os.path.join('../data', 'gagesII_9322_sept30_2011.shp')
+gages = gpd.read_file(file)
+
+# Inspect to be sure it is what I am looking for
+type(gages)
+gages.head()
+gages.columns
+gages.shape
+
+# %%
